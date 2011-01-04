67ebc1c7b627bcb35a64608b3c3e021049a389b0
Khaled Alhourani <me@khaledalhourani.com>
2011-01-04 12:17:01 +0200

    initial commit

---
+++ book_ui.js
@@ -0,0 +1,74 @@
+// $Id$
+(function ($) {
+
+/**
+ * Implementation of Drupal.behaviors for dashboard.
+ */
+Drupal.behaviors.book_ui = {
+  attach: function (context, settings) {
+    //Drupal.behaviors.book_ui.navSlider();
+    Drupal.behaviors.book_ui.paging();
+  },
+
+  /**
+   * Desc
+   */
+  navSlider: function () {
+    var content = $(".node-book .content");
+    var body = $(".node-book .content .field-name-body"); 
+    var nav_menu = $(".block-book .menu li");
+    var node_url = Drupal.settings.base_url + '/node/';
+    var url = Drupal.settings.base_url + '/book_ui/load/';
+
+    var slider = $("<div id='nav_slider'></div>").insertAfter(content).slider({
+      min: 1,
+      max: nav_menu.length,
+      range: "min",
+      value: 1,
+      stop: function(event, ui) {
+        var item = $(nav_menu[ui.value]).find("a");
+        var text = item.text();
+        var href = item.attr("href");
+        var position = href.lastIndexOf('/') + 1;
+        var nid = href.substring(position);
+
+        $.post(url + nid, function(data) {
+          content.html(data);
+          // @todo: handle new url
+          //parent.location.hash = nid;
+        });
+      }
+    });
+  },
+
+  paging: function () {
+    // @todo: substring for words instead of chars
+    var content = $(".node-book .content");
+    var body = $(".node-book .content .field-name-body .field-item");
+
+    var original_height = content.height();
+    var new_height = $(window).height() - 100;
+    var pages_num = Math.ceil(original_height / new_height);
+    var num_words = jQuery.trim(body.text()).split(' ').length;
+    var words_per_page = Math.ceil(num_words / pages_num);
+
+    body.hide();
+    content.append("<div class=\"pages-scroller\"><div class=\"items\"></div></div>");
+    var items = content.find(".items");
+
+    var i = 0;
+    for (i = 0; i < pages_num; i++) {
+      items.append("<div>" + body.text().substring((words_per_page * i), words_per_page) + "</div>");
+    }
+
+    $(".node-book .content .field-name-body").remove();
+
+    //content.height($(window).height() - 100);
+
+    $(window).resize(function(){
+      content.height($(window).height() - 100);
+    });
+  },
+};
+
+})(jQuery);
